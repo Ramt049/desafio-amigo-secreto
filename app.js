77d06e79d2cadfbe06ed2bbd6c57cc7b8c9c3e9d
Ramt049 <luisterrero911@gmail.com>
2025-03-10 12:28:16 -0600

    avance amigo secreto

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// aqui se guardan los nombres de los amigos, declarando la variable
+let listaDeAmigos = [];
+// amigos que participan en le sorteo
+let numeroMaximoDeAmigos = listaDeAmigos;
+
+
+// ingreso de los nombres de los amigos que van a participar 
+function agregarNombreAmigo() {
+    let input = document.getElementById(amigo);
+}
+
