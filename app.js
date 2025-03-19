8a98b42c33db8aa2eb2143a4e0cb91424725cd93
Ramt049 <luisterrero911@gmail.com>
2025-03-18 20:05:54 -0600

    Actualizacion final

--- app.js
+++ app.js
@@ -1,4 +1,3 @@
-// orden sugerido de la aplicación
 //variables
 const inputAmigo = document.getElementById("amigo");
 // aqui se guardan los nombres de los amigos, declarando la variable
@@ -11,11 +10,26 @@ function agregarAmigo() {
   //alerta para que se ingrese un valor en la caja, que no quede vacía y poder continuar
   if (!inputAmigo.value) {
     alert("Para continuar debes ingresar el nombre de tu amigo");
+    return;
   } 
 listaAmigos.push(inputAmigo.value);
 //ulListaAmigos.innerHTML = ulListaAmigos.innerHTML + inputAmigo.value; la siguiente es una acortación de la función, incluyendo el uso de template strings
 ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
+limpiarCaja ();
+return;
 };
+// Esta funcion nos ayuda a limpiar la caja luego de ingresar un nombre
+function limpiarCaja(){
+  // llamar caja con id queryselector
+  document.getElementById("amigo").value = "";
+};
+
+for (let i = 0; i < listaAmigos.length; i++) {
+  const li = document.createElement("li"); 
+  li.textContent = listaAmigos[i];
+  ulListaAmigos.appendChild(li); 
+}
+
 // se asigna la funcion de sortear y se habilita el boton de sorteo del HTML
 function sortearAmigo() {
   // la propiedad length contabiliza los elementos que hay en la lista de amigos que ya es una constante declarada, en conjunto con la funcion math.ramdon nos permite sortear de manera aletoria un elemento ya existen de la lista de amigos
@@ -24,4 +38,5 @@ function sortearAmigo() {
   const amigoSecreto = listaAmigos[random];
   //Nuevamente se usa el recurso del template string para dar el resultado por medio de una constante ya declarada
   ulResultado.innerHTML = `<li> El amigo secreto que te ha tocado es : ${amigoSecreto}</li>`;
-}
+  return;
+};
